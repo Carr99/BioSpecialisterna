ef9bfd9a708f3104b48ca95555acf1a51e743fe5
Carr99 <andre@carrblad.se>
2022-02-22 14:07:48 +0100

    Updated css styling for all html files
    * Updated the css file so all upcoming html files should have the same correct design
    * Updated main.js so it is possible to click on the links in the navbar

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,27 +1,36 @@
 document.querySelector('body').addEventListener('click', function (event) {
 
-    let aButton = event.target.closest('button');
 
-    if (!aButton) { return; }
-    let clickedButton = aButton.getAttribute('id');
+    let aTag = event.target.closest('a');
 
+    // do nothing if not click on an atag
+    if (!aTag) {
+        return;
+    }
+
+    let href = aTag.getAttribute('href');
+
+    // check if external link then open in a new window
+    if (href.indexOf('http') === 0) {
+        aTag.setAttribute('target', '_blank');
+        return;
+    }
 
     event.preventDefault();
-    router(newPage);
+    history.pushState(null, null, href);
+    router();
 });
 
-async function router(aV) {
+async function router() {
     let route = location.pathname;
-    if (aV != null) {
-        route = aV;
-    }
+    console.log(route);
     route = route === '/' ? '/start' : route;
     route = '/html' + route + '.html';
+    console.log(route);
     // load partial
     let content = await (await fetch(route)).text();
     content.includes('<title>Error</title > ') && location.replace(' / ');
     document.querySelector('main').innerHTML = content;
-
 }
 
 window.addEventListener('popstate', router);
