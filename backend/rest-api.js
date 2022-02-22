da57a344974d7a510b59818db9795cbcaaea74e8
pommee <hugoh@hotmail.se>
2022-02-22 11:13:46 +0100

    added frontend, backend and database. Node.JS is also added

---
+++ backend/rest-api.js
@@ -0,0 +1,6 @@
+
+// Import the better-sqlite3 module
+const betterSqlite3 = require('better-sqlite3');
+
+// Connect to a SQLite database
+const db = betterSqlite3('./database/persons.sqlite3');
---
+++ index.js
@@ -0,0 +1,25 @@
+// Port that web server should start on
+// (red from environment variable if it exists or set to 3000)
+const port = process.env.PORT || 3000;
+
+// Import the express module
+const express = require('express');
+
+// Create a web server (app) using express 
+const app = express();
+
+// Serve all the files in the frontend folder
+app.use(express.static('frontend'));
+
+// express.json is built in Express middleware
+// needed to be able to read a request body 
+// (for POST / PUT / PATCH - request)
+app.use(express.json({ limit: '100MB' }));
+
+// Start the web server at port 3000
+app.listen(port, () =>
+  console.log('Listening on http://localhost:' + port));
+
+// Import the rest-api setup function
+const setupRESTapi = require('/backend/rest-api');
+setupRESTapi(app);
