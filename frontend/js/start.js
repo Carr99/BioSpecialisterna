c0c309e5adc61c9e396bbbb27fcde00a68d9c6f4
Lukkah123 <h_lukka@hotmail.com>
2022-03-06 07:55:37 +0100

    Changed to show trailers from actual db
    *Display the trailers in current and upcomming

--- frontend/js/start.js
+++ frontend/js/start.js
@@ -1,21 +1,26 @@
-function getTrailers() {
-  
-let html = `
-      <h2>Current Movies</h2>
-  <iframe width="250" height="200" src="https://www.youtube.com/embed/1NJO0jxBtMo" frameborder="0" allowfullscreen>
-  </iframe>
+async function getTrailers() {
+  let rawData = await fetch('/api/movie/');
+  let movies = await rawData.json();
 
-  <iframe width="250" height="200" src="https://www.youtube.com/embed/owK1qxDselE" frameborder="0" allowfullscreen>
-  </iframe>
+  let htmlCurrent = '';
+  let htmlUpComming = '';
 
-  <iframe width="250" height="200" src="https://www.youtube.com/embed/P9mwtI82k6E" frameborder="0" allowfullscreen>
+  for (let movie of movies) {
+    if(movie.movieId === 1 || movie.movieId === 2)
+      htmlCurrent += `
+      <iframe width="250" height="200" src="${movie.trailerUrl}" frameborder="0" allowfullscreen>
   </iframe>
-  <br>
-  <br>
-    <h2>Upcomming Movies</h2>
-    <iframe width="250" height="200" src="https://www.youtube.com/embed/1NJO0jxBtMo" frameborder="0" allowfullscreen>
+    `
+  }
+
+  for (let movie of movies) { 
+    if(movie.movieId === 3 || movie.movieId === 4)
+    htmlUpComming += `
+      <iframe width="250" height="200" src="${movie.trailerUrl}" frameborder="0" allowfullscreen>
   </iframe>
-    `;
-  
-  document.querySelector('.start').innerHTML = html;
+    `
+  }
+
+  document.querySelector('.current').innerHTML = htmlCurrent;
+  document.querySelector('.upComming').innerHTML = htmlUpComming;
 }
