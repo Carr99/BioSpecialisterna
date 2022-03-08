2c901c77b26e3231b72ea227a8e9baf92742d5ea
Carr99 <andre@carrblad.se>
2022-03-08 18:21:33 +0100

    Added actors and filter button
    * Added so actors also is read from the DB.
    * Created a filter dropdown for the available times.

--- frontend/js/movieInfo.js
+++ frontend/js/movieInfo.js
@@ -1,12 +1,15 @@
 var movieId = 1; //Passed from moviesLister.js
+var allScreenings;
+var screenings;
 
 async function movieInfoLister() {
-  let rawData = await fetch('/api/movie/movieId/' + movieId);
+  let rawData = await fetch('/api/movieActors/movieId/' + movieId);
   let result = await rawData.json();
   printMovieInfo(result);
   let rawDataScreening = await fetch('/api/allMovieScreenings/' + movieId);
-  resultScreening = await rawDataScreening.json();
-  printScreenings(resultScreening);
+  allScreenings = await rawDataScreening.json();
+  screenings = allScreenings;
+  printScreenings();
 }
 
 function printMovieInfo(result) {
@@ -27,15 +30,57 @@ function printMovieInfo(result) {
   </div>
   <p class="movieDesc">` + result.description + `</p>
   <p class="movieDesc"><b>Director: </b>` + result.director + `</p>
-  <p class="movieDesc"><b>Actors: </b>Morgan Freeman, Tim Robbins, Bob Gunton ...</p>`;
+  <p class="movieDesc"><b>Actors: </b>` + result.actors + `</p>`;
   document.querySelector('.section1').innerHTML += htmlSection1;
 }
 
-function printScreenings(screenings) {
-  console.log(screenings);
-  let htmlSection2 = `<h2>Available Times</h2>`;
+function printScreenings() {
+  let htmlSection2 = '';
   for (let screening of screenings) {
     htmlSection2 += '<article class="goBooking" id="infoPage&' + screening.screeningId + '"><div class="columnMargin"><p class="column1">' + screening.date + '</p><p class="column2">' + screening.theaterName + '</p><p class="column3">10 of 100</p></div></article>'
   }
-  document.querySelector('.section2').innerHTML += htmlSection2;
+  document.querySelector('.section2').innerHTML = htmlSection2;
+}
+
+function filterDate() {
+  screenings = allScreenings;
+  let select = document.getElementById('dateFilter');
+  let value = select.options[select.selectedIndex].value;
+  let tempScreenings = screenings;
+  screenings = [];
+
+  let today = new Date();
+  let endDate = new Date()
+
+  if (value == 'all') {
+    screenings = allScreenings;
+    printScreenings();
+    return;
+  } else if (value == 'tomorrow') {
+    endDate.setDate(today.getDate() + 1);
+  } else if (value == 'thisWeek') {
+    endDate.setDate(today.getDate() + 7);
+  } else if (value == 'thisMonth') {
+    endDate.setDate(today.getDate() + 30);
+  }
+
+  today = changeDate(today);
+  endDate = changeDate(endDate);
+  endDate = new Date(endDate);
+
+  for (let screening of tempScreenings) {
+    let screeningDate = screening.date.slice(0, -6)
+    let date = new Date(screeningDate);
+    if (date <= endDate) {
+      screenings.push(screening);
+    }
+  }
+  printScreenings();
+}
+
+function changeDate(date) {
+  let dd = String(date.getDate()).padStart(2, '0');
+  let mm = String(date.getMonth() + 1).padStart(2, '0');
+  let yyyy = date.getFullYear();
+  return yyyy + '-' + mm + '-' + dd;
 }
